9a8ed9d58f257d72464d8e67dc8f33c4d45a9fbb
TArif <alam_tariful@yahoo.com>
2021-07-19 19:42:07 +0600

    update for maede

--- Maede/js/get_style_issues.js
+++ Maede/js/get_style_issues.js
@@ -1,11 +1,53 @@
+//  $(document).ready(function() {
+//      $(".styles").click(function() {
+//          console.log('clicked', this.id)
+//          style = this.id;
+//          switch (style) {
+//              case "M-articles-Default":
+//                  $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+//                  $('link[href="M-articles-Default"]').removeAttr('disabled');
+
+//                  break;
+//              case "M-articles-1700":
+//                  $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+//                 //  $('link[href="M-articles-Default"]').attr('disabled', 'true');
+//                  $('link[href="M-articles-1700"]').removeAttr('disabled');
+
+
+//                  break;
+//              case "M-articles-1800":
+//                  $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+//                 //  $('link[href="M-articles-Default"]').attr('disabled', 'true');
+//                  $('link[href="M-articles-1800"]').removeAttr('disabled');
+
+//                  break;
+//              case "M-articles-1900":
+//                  $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+//                 //  $('link[href="M-articles-Default"]').attr('disabled', 'true');
+//                  $('link[href="M-articles-1900"]').removeAttr('disabled');
+
+//                  break;
+//              case "M-articles-2021":
+//                  $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+//                 //  $('link[href="M-articles-Default"]').attr('disabled', 'true');
+//                  $('link[href="M-articles-2021"]').removeAttr('disabled');
+
+//                  break;
+
+//              default:
+//                  console.log("ti prego non comparire");
+//          }
+//      });
+//  });
+
  $(document).ready(function() {
      $(".styles").click(function() {
          console.log('clicked')
          style = this.id;
          switch (style) {
-             case "reset":
+             case "default":
                  $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
-                 $('link[href="assets/css/main.css"]').removeAttr('disabled');
+                 $('link[href="assets/css/default.css"]').removeAttr('disabled');
 
                  break;
              case "home1700":
@@ -27,9 +69,16 @@
                  $('link[href="assets/css/home1900.css"]').removeAttr('disabled');
 
                  break;
+             case "home2021":
+                 $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+                 $('link[href="assets/css/main.css"]').attr('disabled', 'true');
+                 $('link[href="assets/css/home2021.css"]').removeAttr('disabled');
+
+                 break;
 
              default:
-                 console.log("ti prego non comparire");
+                 $('link[rel="stylesheet alternative"]').attr('disabled', 'true');
+                 $('link[href="assets/css/default.css"]').removeAttr('disabled');
          }
      });
  });
